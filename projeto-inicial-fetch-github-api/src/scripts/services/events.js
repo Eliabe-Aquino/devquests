054ed1e461eb9770d2cd03450ce5339fafd35a09
Eliabe Aquino <13698848+Eliabe-Aquino@users.noreply.github.com>
2023-09-14 19:12:33 -0300

    Delete projeto-inicial-fetch-github-api directory

--- projeto-inicial-fetch-github-api/src/scripts/services/events.js
+++
@@ -1,9 +0,0 @@
-import {baseUrl, eventsQuantity} from '/src/scripts/variables.js'
-
-async function getEvents(userName) {
-    const response = await fetch(`${baseUrl}/${userName}/events?per_page=${eventsQuantity}`)
-    return await response.json()
-}
-
-export { getEvents }
-
--- projeto-inicial-fetch-github-api/src/scripts/variables.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = 'https://api.github.com/users'
-
-const respositoriesQuantity = 10
-const eventsQuantity= 10
-
-export {baseUrl, respositoriesQuantity, eventsQuantity}
