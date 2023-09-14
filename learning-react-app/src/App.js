1f7b95dc2fc8b3a650afe21426d42771a84afcc0
Eliabe Aquino <13698848+Eliabe-Aquino@users.noreply.github.com>
2023-09-14 15:45:44 -0300

    Add files via upload

---
+++ learning-react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Paragraph from './components/paragraph/paragraph';
+import Button from './components/button/button'
+
+function App() {
+  return (
+    <>
+    <Paragraph />
+    <Button label='Baixar CV' />
+    </>
+  );
+}
+
+export default App;
+
+
